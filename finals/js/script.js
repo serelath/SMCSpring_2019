20f2169f3eba2a06ff368f92018ca8327a3fed7c
serelath <shoh@go.pasadena.edu>
2019-06-02 23:12:31 -0700

    6 product pages done and create menu

--- finals/js/script.js
+++ finals/js/script.js
@@ -35,12 +35,57 @@ $('.popupbg, .fa-times').click(function() {
 	$('.popupbg').removeClass('popupOut'); }, 200);
 });
 
-//cart add
+//cart empty
 var itemNum = 0;
+	
 if (itemNum == 0) {
 	$('.empty').show();
+	$('.checkout').addClass('disable-checkout');
 } else {
 	$('.empty').hide();
+	$('.checkout').removeClass('disable-checkout');
 }
 	
-})
+// click item
+$('.options div').click(function() {
+	
+	itemNum++;
+	
+	if (itemNum == 0) {
+		$('.empty').show();
+		$('.checkout').addClass('disable-checkout');
+	} else {
+		$('.empty').hide();
+		$('.checkout').removeClass('disable-checkout');
+	}
+	
+	var item = $(this).parent().siblings('.label').find('h5').text();
+	var cost = $(this).find('h5').text();
+	var oz = $(this).find('.amount').text();
+	var img = $(this).parent().parent().parent().find('.image').attr('src');
+	
+	$('.popup').append(
+		'<div class="item row mt-5">' + '<div class="col-3">' + '<a href="#" class="">' + '<img src=' + img + ' alt="" class="w-100">' + '</a>' + '</div>' + '<div class="col-8">' + '<a href="#" class="item-label">' + '<p class="mb-0">' + '<strong>' + item + oz + '</strong>' + '</p>' + '</a>' + '<p class="mb-0 price">' + cost + '</p>' + '</div>' + '<i class="fas fa-times"></i>' + '</div>'
+	);
+	
+	$('.itemNum').html(itemNum);
+	
+});
+	
+//remove item
+$('body').on('click', '.item i', function() {
+	
+	itemNum--;
+	
+	if (itemNum == 0) {
+		$('.empty').show();
+		$('.checkout').addClass('disable-checkout');
+	} else {
+		$('.empty').hide();
+		$('.checkout').removeClass('disable-checkout');
+	}
+	
+	$(this).parent().remove();
+	
+});
+});
